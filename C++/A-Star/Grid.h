fca5e51a6c54445a6b80813b757047a3f5aa36d7
Seyed Sobhan Hosseini <149086622+Sobhan-Hosseini@users.noreply.github.com>
2024-08-23 15:18:47 +0330

    Update Grid.h

--- C++/A-Star/Grid.h
+++ C++/A-Star/Grid.h
@@ -1,3 +1,9 @@
+/*
+  Map Grids Class
+  Authors: Seyed Sobhan Hosseini - Amirhossein Gholizadeh
+  Date: ‎Friday, ‎August ‎23, ‎2024, ‏‎3:09:17 PM
+*/
+
 #include <iostream>
 #include <cstdint>
 #include <math.h>
